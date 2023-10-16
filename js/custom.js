fef0651d27e96a6b07f9e6db1a25e4fc6a2f6de6
Md <sohag.wd.bd@gmail.com>
2023-10-16 20:20:22 +0600

    buy website

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+//banner part
+    $('.banner_main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed:1500,
+        arrows:true,
+        prevArrow:'.left',
+        nextArrow:'.right',
+      });
+});
